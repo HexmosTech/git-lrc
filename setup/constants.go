fedeb6810fc70c9a98d67b6932accf85bcdb0e4a
Lince Mathew <linz07m@gmail.com>
2026-09-05 14:48:56 +0530

    Update default gemini models
    LiveReview Pre-Commit Check: ran (iter:1, coverage:0%)

--- setup/constants.go
+++ setup/constants.go
@@ -1,10 +1,10 @@
 package setup
 
 const (
-	CloudAPIURL        = "https://livereview.hexmos.com"
-	HexmosSigninBase   = "https://hexmos.com/signin"
-	GeminiKeysURL      = "https://aistudio.google.com/api-keys"
-	DefaultGeminiModel       = "gemini-2.5-flash"
-	DefaultGeminiHelperModel = "gemini-2.5-flash-lite"
-	IssuesURL          = "https://github.com/HexmosTech/git-lrc/issues/new"
+	CloudAPIURL              = "https://livereview.hexmos.com"
+	HexmosSigninBase         = "https://hexmos.com/signin"
+	GeminiKeysURL            = "https://aistudio.google.com/api-keys"
+	DefaultGeminiModel       = "gemini-3.6-flash"
+	DefaultGeminiHelperModel = "gemini-3.5-flash-lite"
+	IssuesURL                = "https://github.com/HexmosTech/git-lrc/issues/new"
 )
